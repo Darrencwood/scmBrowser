730134604d1a65110b0ca86132f9a7084470a19a
Marcelo Rodriguez <marcelo@rodriguez.com.mx>
2016-10-25 12:40:10 -0500

    Fixed issue with host header value from proxyRegister

--- app.js
+++ app.js
@@ -6,14 +6,10 @@ var express  = require('express'),
 		bodyParser = require('body-parser'),
 		methodOverride = require('method-override'),
 		httpProxy = require('http-proxy'),
-		path = require('path'); 
+		path = require('path'),
+		url=require('url');; 
 
-var apiProxy = httpProxy.createProxyServer({
-  secure: false,
-  headers: {
-    host: 'cc.vlab.test'
-  }
-});
+var apiProxy = undefined;
 
 var target = '';
 
@@ -28,7 +24,44 @@ proxyRouter.post('/registerUrl', function(req, res) {
 	//console.log(req);
 	if (!req.body || !req.body.url) return res.sendStatus(400);
 	target = req.body.url;
+	let host = url.parse(target).hostname;
 	console.log('Reconfigure target to : ' + target);
+	console.log('Reconfigure host to : ' + host);
+	// Create apiProxy object
+	apiProxy = httpProxy.createProxyServer({
+    secure: false,
+    headers: {
+      host: host
+    }
+  });
+  
+  apiProxy.on('proxyReq', function(proxyReq, req, res, options) {
+	  console.log('---------- proxyReq -------------');
+	  console.log(req.headers.origin);
+	  console.log(req.method);
+	  console.log(req.url);
+	  console.log(req.body);
+	  if(req.body) {
+	    let bodyData = JSON.stringify(req.body);
+	    proxyReq.setHeader('Content-Type', 'application/json');
+	    proxyReq.setHeader('Content-Lenght', Buffer.byteLength(bodyData));
+	    proxyReq.write(bodyData);
+	  }
+  });
+
+  apiProxy.on('proxyRes', function(proxyRes, req, res, options) {
+  	console.log('---------- proxyRes -------------');
+  	console.log(JSON.stringify(proxyRes.headers, true, 2));
+  	console.log(JSON.stringify(proxyRes.url, true, 2));
+  	console.log(JSON.stringify(proxyRes.body, true, 2));
+  });
+  
+  
+  apiProxy.on('error', function (err, req, res) {
+  	console.log('error:');
+  	console.log(err);
+  });
+	
 	return res.sendStatus(200);
 });
 
@@ -42,39 +75,13 @@ app.use(methodOverride());
 app.use('/node_modules',  express.static( path.join(__dirname, '/node_modules')));
 app.use('/proxy', proxyRouter);
 app.all('/api/*', function(req, res) {
+
 	if ( target != '') {
   	apiProxy.web(req, res, { target: target});
   } else {
   	return res.sendStatus(400);
   }
 });                                       
-
-apiProxy.on('proxyReq', function(proxyReq, req, res, options) {
-	console.log('---------- proxyReq -------------');
-	console.log(req.headers);
-	console.log(req.method);
-	console.log(req.url);
-	console.log(req.body);
-	if(req.body) {
-	  let bodyData = JSON.stringify(req.body);
-	  proxyReq.setHeader('Content-Type', 'application/json');
-	  proxyReq.setHeader('Content-Lenght', Buffer.byteLength(bodyData));
-	  proxyReq.write(bodyData);
-	}
-});
-
-apiProxy.on('proxyRes', function(proxyRes, req, res, options) {
-	console.log('---------- proxyRes -------------');
-	console.log(JSON.stringify(proxyRes.headers, true, 2));
-	console.log(JSON.stringify(proxyRes.url, true, 2));
-	console.log(JSON.stringify(proxyRes.body, true, 2));
-});
-
-
-apiProxy.on('error', function (err, req, res) {
-	console.log('error:');
-	console.log(err);
-});
 
 app.listen(8888);
 console.log("App listening on port 8888");
